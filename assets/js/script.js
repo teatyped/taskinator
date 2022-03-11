3b034ff6bbe053620bbe6f4d8a69903678c9b4f2
Khai Truong <kminht@gmail.com>
2022-03-11 11:34:34 -0500

    submit button/ selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,20 @@
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form");
 
-var createTaskHandler = function(){
+var createTaskHandler = function(event){
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
+
 }
 // summery oh above Create a new task item.//Style the new task item.//Add the text.// Append this element to the task list.
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
 
 
