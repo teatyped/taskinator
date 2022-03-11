86a766d737a3100edc6a6fdfc089326ea49acfa4
Khai Truong <kminht@gmail.com>
2022-03-11 13:00:59 -0500

    from inputtype disply

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,26 @@ var createTaskHandler = function(event){
 
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+// create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+// create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+// give it a class name
+    taskInfoEl.className = " task-info";
+// add Html context to div 
+    taskInfoEl.innerHTML = "<h3 class'task-name'>" + taskNameInput+ "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+// add entire list item to list 
     tasksToDoEl.appendChild(listItemEl);
 
+    console.dir(listItemEl);
+
 }
-// summery oh above Create a new task item.//Style the new task item.//Add the text.// Append this element to the task list.
 
 formEl.addEventListener("submit", createTaskHandler);
 
